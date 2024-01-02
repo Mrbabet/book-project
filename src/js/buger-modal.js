4584e78ea98b0f94c33169e19fec346ffec50ba7
Igor <igor_socha@yahoo.com>
2024-01-02 19:32:28 +0100

    v.8.1  menu

--- src/js/buger-modal.js
+++ src/js/buger-modal.js
@@ -1,15 +1,17 @@
-// Funkcja otwierająca modal i zmieniająca ikony
+// Funkcja otwierająca modal i blokująca scrollowanie strony
 function openModal() {
   document.getElementById('burgerModal').style.display = 'block';
   document.querySelector('.btn-burger').style.display = 'none';
   document.querySelector('.btn-close').style.display = 'block';
+  document.body.style.overflow = 'hidden'; // Blokuje scrollowanie strony
 }
 
-// Funkcja zamykająca modal i zmieniająca ikony
+// Funkcja zamykająca modal i przywracająca scrollowanie strony
 function closeModal() {
   document.getElementById('burgerModal').style.display = 'none';
   document.querySelector('.btn-burger').style.display = 'block';
   document.querySelector('.btn-close').style.display = 'none';
+  document.body.style.overflow = 'auto'; // Przywraca scrollowanie strony
 }
 
 // Obsługa zdarzeń dla przycisków burgera i zamykania

66af0c054d622087765908ef34d015d9b3b5b2a1
Mrbabet <wieliczkokamil@gmail.com>
2024-01-03 09:54:53 +0100

    Update loader.js

--- src/js/loader.js
+++ src/js/loader.js
@@ -10,7 +10,7 @@ export function showLoadingIndicator() {
   Loading.pulse({
     clickToClose: true,
     svgSize: '100px',
-    backgroundColor: 'rgba(17, 17, 17, 0.3)',
+    backgroundColor: 'rgba(17, 17, 17, 1)',
     svgColor: '#4f2ee8',
   });
 }

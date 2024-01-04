c7551986548e3d5ce0a5d33fec2bfff18f64d77b
Mrbabet <wieliczkokamil@gmail.com>
2024-01-04 10:28:35 +0100

    working on fixing burder modal displaying properly

--- src/js/auth/auth-form.js
+++ src/js/auth/auth-form.js
@@ -24,6 +24,7 @@ const mobileMenuContainer = document.querySelector('.mobile-modal-menu');
 const mobileSignUpBtn = document.querySelector('.modal-sign-up');
 const mobileUsername = document.querySelector('.avatar-p');
 const mobileLogOutBtn = document.querySelector('.menu-log-out ');
+const avatar = document.querySelector('.avatar');
 
 const onSignUpBtnClick = function () {
   submitBtn.textContent = 'Sign up';
@@ -67,6 +68,7 @@ window.addEventListener('load', () => {
       mobileSignUpBtn.classList.add('is-authenticated');
       mobileLogOutBtn.classList.add('is-authenticated');
       mobileMenuContainer.classList.add('is-authenticated');
+      avatar.classList.add('is-authenticated');
       headerSignUpBtn.textContent = user.displayName;
       mobileUsername.textContent = user.displayName;
       headerSignUpBtn.addEventListener('click', userSignOut);
@@ -74,6 +76,7 @@ window.addEventListener('load', () => {
     } else {
       hideModal();
       menuContainer.classList.remove('is-authenticated');
+      avatar.classList.remove('is-authenticated');
       headerSignUpBtn.innerHTML = `<p class="sign-up-p">Sign up</p>
       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
       <path d="M3.33325 10H16.6666M16.6666 10L11.6666 5M16.6666 10L11.6666 15" stroke="#EAC645" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
